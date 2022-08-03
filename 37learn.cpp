1b46dfc23499f4b4645636ce875230a93a23804e
Partha Paul <pparthapaul11@gmail.com>
2022-08-03 19:27:27 +0530

    queue

---
+++ 37learn.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+//TOPIC Queue -- operations ->enqueue, dequeue , peek, empty
+
+class queue{
+    public:
+        int* arr;
+        int* front;
+        int* back;
+        queue(){
+
+        }
+
+};
+int main(){
+ 
+}
